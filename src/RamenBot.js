3cdb01424e9578cd7b9a920de2c207ef07410a5b
annoor98 <annoor98@hotmail.com>
2020-05-13 04:10:35 -0600

    Added simple weather command

--- src/RamenBot.js
+++ src/RamenBot.js
@@ -1,7 +1,12 @@
+//We are using eris to manage the bot
 const eris = require('eris');
-
+//We are using node-fetch to fetch data from public APIs
+const fetch = require('node-fetch');
 //Put bot token here
 const bot = new eris.Client('');
+//Put Open Weather Api Key and city key here
+const weatherKey = '';
+const cityId = '';
 
 bot.on('ready', () => {
   console.log('Connection Successful!');
@@ -23,6 +28,29 @@ bot.on('messageCreate', async (msg) =>{
   }
 
 });
+
+bot.on('messageCreate', (msg) => {
+    if(msg.content === 'show me weather'){
+      getWeather(msg);
+    }
+});
+
+function getWeather(msg){
+  var message = "Sorry dude I couldn't get the weather.";
+  var apiCall = 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + "&APPID="
+                + weatherKey + "&&units=metric";
+
+  fetch(apiCall).then(res => res.json()).then(data => {
+    var temp = data['main']['temp'];
+    var feels = data['main']['feels_like'];
+    var description = data['weather'][0]['description'];
+
+    message = "The temperature right now is " + temp + "°C. Feels like " +
+              feels + "°C.\nWeather Type: " + description;
+    bot.createMessage(msg.channel.id, message);
+
+  }).catch(err => bot.createMessage(msg.channel.id, message));
+}
 
 bot.on('error', err => {
   console.warn(err);
